7fbf2d30171476916ea469267c82d2057a01a040
AkiTheMemeGod <142716100+AkiTheMemeGod@users.noreply.github.com>
2023-11-26 11:48:48 +0530

    urgent commit

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ def get_todos(filepath="todos.txt"):
     return todos
 
 
+
 def put_todos(tds, filepath="todos.txt"):
     """This function helps to put the todos you enter into a text file"""
     with open(filepath, 'w') as file:
